2d8304b1f2b6c23ad83827323ad6d7f960e58884
addleton <michaeladdleton1@gmail.com>
2024-01-28 20:12:07 +0000

    changes styling to single game page

--- src/components/GameScorePage.tsx
+++ src/components/GameScorePage.tsx
@@ -31,98 +31,27 @@ export const GameScorePage: React.FC = () => {
     return <h2>loading</h2>;
   } else {
     return (
-      <div className="hero min-h-screen min-w-screen bg-base-100">
-        <div className="hero-content flex-col box-content bg-base-200 shadow-xl rounded-lg min-w-96">
+      <div className="hero min-h-screen min-w-screen bg-base-100 flex content-center justify-evenly">
+        <div className="hero-content flex-col box-content bg-base-100 shadow-xl rounded-lg min-w-96">
           <h1 className="text-4xl font-bold">{game.name}</h1>
           <div className="hero-content flex-col lg:flex-row">
             <img
               src={game.img}
-              className="max-w-2xl rounded-lg shadow-2xl mr-12 ml-12 flex-shrink-0"
+              className="max-w-2xl rounded-lg shadow-2xl flex-shrink-0"
             />
             <div className="divider lg:divider-horizontal" />
-            <div className="container ml-12 mr-12 flex flex-col self-start flex-grow">
-              <h3 className="text-xl font-bold">Gameplay</h3>
-
-              <div className="flex mb-4">
-                <div className="rating rating-md mr-2">
-                  <input
-                    type="radio"
-                    name="rating-8"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-                <h3 className="text-xl font-bold">{game.avg_gameplay}</h3>
-              </div>
-              <h3 className="text-xl font-bold">Narrative</h3>
-
-              <div className="flex mb-4">
-                <div className="rating rating-md mr-2">
-                  <input
-                    type="radio"
-                    name="rating-8"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-                <h3 className="text-xl font-bold">{game.avg_narrative}</h3>
-              </div>
-              <h3 className="text-xl font-bold ">Soundtrack / Score</h3>
-
-              <div className="flex mb-4">
-                <div className="rating rating-md mr-2">
-                  <input
-                    type="radio"
-                    name="rating-8"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-                <h3 className="text-xl font-bold">{game.avg_soundtrack}</h3>
-              </div>
-              <h3 className="text-xl font-bold">Art Direction</h3>
-
-              <div className="flex mb-4">
-                <div className="rating rating-md mr-2">
-                  <input
-                    type="radio"
-                    name="rating-8"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-                <h3 className="text-xl font-bold">{game.avg_art_direction}</h3>
-              </div>
-              <h3 className="text-xl font-bold ">Personal Enjoyment</h3>
-
-              <div className="flex mb-4">
-                <div className="rating rating-md mr-2">
-                  <input
-                    type="radio"
-                    name="rating-8"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-                <h3 className="text-xl font-bold">{game.avg_enjoyment}</h3>
-              </div>
-              <h3 className="text-4xl font-bold">Final Score</h3>
-
-              <div className="flex mb-4">
-                <div className="rating rating-lg mr-2">
-                  <input
-                    type="radio"
-                    name="rating-8"
-                    className="mask mask-star-2 bg-orange-400"
-                  />
-                </div>
-                <h3 className="text-4xl font-bold">{game.avg_final_score}</h3>
-              </div>
+            <div className="container flex flex-col self-start flex-grow min-w-96">
+              <p className="text-md font-bold">{game.description}</p>
             </div>
           </div>{" "}
           <div className="divider"></div>
-          <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="hero-content flex-col lg:flex-row-reverse w-full content-evenly justify-evenly">
             <img
               src={game.alt_img}
-              className="max-w-2xl rounded-lg shadow-2xl mr-12 ml-12 flex-shrink-0"
+              className="max-w-2xl rounded-lg shadow-2xl w-full justify"
             />
             <div className="divider lg:divider-horizontal" />
-            <div className="container ml-12 mr-12 flex flex-col self-start flex-grow">
+            <div className="container flex-col max-w-36">
               <h3 className="text-xl font-bold ">Genre(s)</h3>
 
               <div className="flex-col mb-4">
@@ -172,6 +101,82 @@ export const GameScorePage: React.FC = () => {
                 <h3 className="text-sm font-bold">{game.released}</h3>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="divider lg:divider-horizontal" />
+
+        <div className="flex-col ">
+          <h3 className="text-xl font-bold">Gameplay</h3>
+
+          <div className="flex mb-4">
+            <div className="rating rating-md mr-2">
+              <input
+                type="radio"
+                name="rating-8"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+            <h3 className="text-xl font-bold">{game.avg_gameplay}</h3>
+          </div>
+          <h3 className="text-xl font-bold">Narrative</h3>
+
+          <div className="flex mb-4">
+            <div className="rating rating-md mr-2">
+              <input
+                type="radio"
+                name="rating-8"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+            <h3 className="text-xl font-bold">{game.avg_narrative}</h3>
+          </div>
+          <h3 className="text-xl font-bold ">Soundtrack / Score</h3>
+
+          <div className="flex mb-4">
+            <div className="rating rating-md mr-2">
+              <input
+                type="radio"
+                name="rating-8"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+            <h3 className="text-xl font-bold">{game.avg_soundtrack}</h3>
+          </div>
+          <h3 className="text-xl font-bold">Art Direction</h3>
+
+          <div className="flex mb-4">
+            <div className="rating rating-md mr-2">
+              <input
+                type="radio"
+                name="rating-8"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+            <h3 className="text-xl font-bold">{game.avg_art_direction}</h3>
+          </div>
+          <h3 className="text-xl font-bold ">Personal Enjoyment</h3>
+
+          <div className="flex mb-4">
+            <div className="rating rating-md mr-2">
+              <input
+                type="radio"
+                name="rating-8"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+            <h3 className="text-xl font-bold">{game.avg_enjoyment}</h3>
+          </div>
+          <h3 className="text-4xl font-bold">Final Score</h3>
+
+          <div className="flex mb-4">
+            <div className="rating rating-lg mr-2">
+              <input
+                type="radio"
+                name="rating-8"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+            <h3 className="text-4xl font-bold">{game.avg_final_score}</h3>
           </div>
           {hasScored ? null : (
             <Link to={"add-score"}>
