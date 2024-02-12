0715cb4fb85f0068cb757e8f2a9abeb14c31c987
addleton <michaeladdleton1@gmail.com>
2024-02-12 14:36:15 +0000

    adds mobile styling to addscorepage

--- src/components/AddScorePage.tsx
+++ src/components/AddScorePage.tsx
@@ -7,6 +7,7 @@ import { Soundtrack } from "./StarRatings/Sountrack";
 import { UserContext } from "../context/UserContext";
 import { addScore, getGameFromFirestore } from "../utils/gamesApi";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { resizeFunction } from "../utils/utils";
 
 export const AddScorePage: React.FC = () => {
   const [game, setGame] = useState(null);
@@ -17,6 +18,7 @@ export const AddScorePage: React.FC = () => {
   const [artScore, setArtScore] = useState(0);
   const [enjoymentScore, setEnjoymentScore] = useState(0);
   const { user } = useContext(UserContext);
+  const [screenSize, setScreenSize] = useState("desktop");
   const { id } = useParams();
   const navigate = useNavigate();
   const handleAddGame = async () => {
@@ -52,16 +54,23 @@ export const AddScorePage: React.FC = () => {
     retrieveGame();
   }, []);
 
+  useEffect(() => {
+    const resize = resizeFunction(setScreenSize);
+    return () => {
+      resize();
+    };
+  }, []);
+
   if (isLoading) {
     return (
       <div className="hero min-h-screen">
         <span className="loading loading-spinner text-primary"></span>
       </div>
     );
-  } else {
+  } else if (screenSize === "desktop") {
     return (
       <>
-        {user ? (
+        {user !== null || user !== undefined ? (
           <div className="hero min-h-screen bg-base-100">
             <div className="hero-content flex-col lg:flex-row box-content bg-base-200 shadow-xl rounded-lg p-20">
               <img
@@ -97,6 +106,73 @@ export const AddScorePage: React.FC = () => {
                   <ArtDirection artScore={artScore} setArtScore={setArtScore} />
                 </div>
                 <h3 className="text-xl font-bold">Personal Enjoyment</h3>
+                <Enjoyment
+                  enjoymentScore={enjoymentScore}
+                  setEnjoymentScore={setEnjoymentScore}
+                />
+                <button className="btn mt-12" onClick={handleAddGame}>
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : !user ? (
+          <Link to="/login">
+            <p>login</p>
+          </Link>
+        ) : (
+          <p>select a game first</p>
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        {user !== null || user !== undefined ? (
+          <div>
+            <div className="hero-content flex-col   mt-10">
+              <h2 className="text-2xl mb-6 font-bold mobile-add-game-text">
+                {game.name}
+              </h2>
+              <img src={game.img} className="mobile-add-game-image" />
+              <div className="divider lg:divider-horizontal" />
+              <div className="flex flex-col self-center">
+                <h3 className="text-lg font-bold mobile-add-game-text">
+                  Gameplay
+                </h3>
+                <div>
+                  <Gameplay
+                    gameplayScore={gameplayScore}
+                    setGameplayScore={setGameplayScore}
+                  />
+                </div>
+                <h3 className="text-lg font-bold mobile-add-game-text">
+                  Narrative
+                </h3>
+                <div>
+                  <Narrative
+                    narrativeScore={narrativeScore}
+                    setNarrativeScore={setNarrativeScore}
+                  />
+                </div>
+                <h3 className="text-lg font-bold mobile-add-game-text">
+                  Soundtrack / Score
+                </h3>
+                <div>
+                  <Soundtrack
+                    soundScore={soundScore}
+                    setSoundScore={setSoundScore}
+                  />
+                </div>
+                <h3 className="text-lg font-bold mobile-add-game-text">
+                  Art Direction
+                </h3>
+                <div>
+                  <ArtDirection artScore={artScore} setArtScore={setArtScore} />
+                </div>
+                <h3 className="text-lg font-bold mobile-add-game-text">
+                  Personal Enjoyment
+                </h3>
                 <Enjoyment
                   enjoymentScore={enjoymentScore}
                   setEnjoymentScore={setEnjoymentScore}
