817ddeecf87a10579f97d3664bcf2175f0164c0a
addleton <michaeladdleton1@gmail.com>
2024-02-13 10:52:50 +0000

    aligns mobile homepage view to be 90vw

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -279,7 +279,7 @@ const Home = ({
               </div>
             </div>
           ) : (
-            <div className="mobile-logged-out-homepage-container">
+            <div className="mobile-logged-out-homepage-container mx-auto">
               <div className="mobile-logged-out-homepage">
                 <h1 className="text-5xl font-bold">Welcome to myGameScores</h1>
                 <p className="py-6">
